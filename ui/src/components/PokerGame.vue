09cab55063c43098ed6ed12015c592885f51c0e8
panchen <451161722@qq.com>
2023-12-13 23:11:29 +0800

    玩家一的牌显示完成

--- ui/src/components/PokerGame.vue
+++ ui/src/components/PokerGame.vue
@@ -1,13 +1,42 @@
 <template>
   <div class="poker-ganme">
     <div class="table">
-      <div class="top-table">顶部三张牌</div>
-      <div class="player-pokers">玩家手牌</div>
-      <div class="hands-card-left">左侧玩家手牌
-        <div class="back">背景图</div>
+      <!-- 顶部三张牌 -->
+      <div class="top-table">
+        <el-card shadow="hover" :body-style="{ padding: '0px' }">
+          <img src="../images/cardback.png" class="image" />
+        </el-card>
+        <el-card shadow="hover" :body-style="{ padding: '0px' }">
+          <img shadow="hover" src="../images/cardback.png" class="image" />
+        </el-card>
+        <el-card shadow="hover" :body-style="{ padding: '0px' }">
+          <img src="../images/cardback.png" class="image" />
+        </el-card>
       </div>
-      <div class="hands-card-right">右侧玩家手牌
-        <div class="back">背景图</div>
+      <!-- 玩家手牌 -->
+      <div class="player-pokers" >
+        <el-card shadow="hover" :body-style="{ padding: '0px' }" class="player-card"
+          v-for="(poker, index) in player1.hands"
+          :key="poker.id"
+          :style="{ left: index * 20 + 'px' }"
+          :isSelected="poker.isSelected">
+          <img :src="`/src/images/${poker.id}.png`" class="image" :style="{ width: '120px', height: 'auto' }"/>
+        </el-card>
+      </div>
+      <!-- 左侧手牌 -->
+      <div class="hands-card-left">
+        <el-card shadow="hover" :body-style="{ padding: '0px' }">
+          <img shadow="hover" src="../images/cardback.png" class="image" />
+        </el-card>
+        <!-- 左侧玩家背景 -->
+        <div class="back"></div>
+      </div>
+      <div class="hands-card-right">
+        <el-card shadow="hover" :body-style="{ padding: '0px' }">
+          <img shadow="hover" src="../images/cardback.png" class="image" />
+        </el-card>
+        <!-- 右侧玩家背景 -->
+        <div class="back"></div>
       </div>
     </div>
     <div class="player-list">
@@ -34,14 +63,41 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import PokerCard from './PokerCard.vue';
+//import PokerCard from './PokerCard.vue';
+import POKERS from '../constant/poker';
+import type { PokerCard } from '../constant/poker';
+import type { Ref } from 'vue';
 
-const gamePrepared = ref(false);
-const gameStarted = ref(true);
-const gamePlayed = ref(true);
+const gamePrepared = ref(false);//已经准备
+const gameStarted = ref(true);//已经开始
+const gamePlayed = ref(true);//已经玩了
+
+interface Player {
+  name: string,//名字
+  hands: PokerCard[],//手牌
+  isLandlord: boolean//地主
+}
+
+let player1: Ref<Player> = ref({
+  name: 'Alice',
+  hands: [],
+  isLandlord: false
+});
+
+function getRandomCards(cards: PokerCard[], count: number): PokerCard[] {
+  let shuffled = cards.slice();
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled.slice(0, count);
+}
+
 const prepareGame = () => {
   console.log("准备游戏");//准备游戏
   gamePrepared.value = true;
+  player1.value.hands = getRandomCards(POKERS, 17);
+  console.log(player1.value.hands);
 }
 const playCards = () => {
   gamePlayed.value = true;
@@ -65,6 +121,55 @@ const skipCallGame = () => {
   /* 全屏高度 */
   display: flex;
   flex-direction: column;
+
+  .table {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 20;
+
+    .top-table {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      transform: translate(-50%, 0);
+      display: flex;
+    }
+
+    .player-pokers {
+      height: 31.5vh;
+      position: absolute;
+      left: 50%;
+      bottom: 0.5vw;
+      transform: translate(-50%, 0);
+      display: flex;
+      width: calc(16 * 20px + 120px);
+      .player-card{
+        position: absolute;
+      }
+    }
+
+    .hands-card-left {
+      position: absolute;
+      left: 20vw;
+      top: 40vh;
+      width: 9vh;
+      height: 12vh;
+      padding: 0;
+    }
+
+    .hands-card-right {
+      position: absolute;
+      right: 20vw;
+      top: 40vh;
+      width: 9vh;
+      height: 12vh;
+      padding: 0;
+    }
+  }
 }
 
 .btn-group {
--- ui/src/constant/poker.ts
+++ ui/src/constant/poker.ts
@@ -1,10 +1,9 @@
-type Suit = "spades" | "hearts" | "diamonds" | "clubs" | "big" | "little";
+type Suit = "spades" | "hearts" | "diamonds" | "clubs" | "red" | "black";
 type Rank = "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A" | "JOKER";
 type SortKey = string;
 
-interface PokerCard {
+export interface PokerCard {
   id: string;
-  rank: Rank;
   suit: Suit;
   sortKey: SortKey;
   isSelected: boolean;
@@ -17,8 +16,7 @@ let POKERS: PokerCard[] = [];
 NUMS.forEach((num) => {
   SUITS.forEach((suit) => {
     POKERS.push({
-      id: num + suit,
-      rank: num,
+      id: num + '_of_' + suit,
       suit: suit,
       sortKey: num,
       isSelected: false,
@@ -29,19 +27,38 @@ NUMS.forEach((num) => {
 // Add jokers
 POKERS = POKERS.concat([
   {
-    rank: "JOKER",
-    suit: "big",
-    sortKey: "big",
-    id: "JOKERbig",
+    id: "red_joker",
+    suit: "red",
+    sortKey: "red",
     isSelected: false,
   },
   {
-    rank: "JOKER",
-    suit: "little",
-    sortKey: "little",
-    id: "JOKERlittle",
+    id: "black_joker",
+    suit: "black",
+    sortKey: "black",
     isSelected: false,
   },
 ]);
+
+function createPokerCard(id:string):PokerCard{
+  let suit: Suit;
+  let sortKey: SortKey;
+  const isSelected = false; // 默认值，可以根据需要调整
+  // 示例逻辑：根据id确定suit和sortKey
+  // 这里的逻辑应该根据你的具体需求来定制
+  if (id === "特定ID1") {
+      suit = 'spades';
+      sortKey = '2'; 
+  } else if (id === "特定ID2") {
+      suit = 'spades';
+      sortKey = "3"; 
+  } else {
+      // 默认值或错误处理
+      suit = 'spades';
+      sortKey = '2';
+  }
+
+  return { id, suit, sortKey, isSelected };
+}
 
 export default POKERS;
