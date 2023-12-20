07609961f2bc809ae6048193c2adc74ae41c35ea
Icer Liang <liangshuang@sutu.tech>
2023-12-21 07:47:21 +0800

    copy circuits to ui

--- circuits/scripts/cpcircuit.sh
+++ circuits/scripts/cpcircuit.sh
@@ -6,10 +6,7 @@ CIRCUIT_NAME=${CIRCUIT_NAME:=test}
 
 set -e # stop for errors
 
-cp ${FULL_PATH}_obj/verifier.sol src/contracts/${CIRCUIT_NAME}Verifier.sol
+mkdir -p ../ui/public/circuits
 
-mkdir -p ui/public/circuits
-
-cp ${FULL_PATH}_obj/${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm ui/public/circuits/${CIRCUIT_NAME}.wasm
-cp ${FULL_PATH}_obj/${CIRCUIT_NAME}_final.zkey ui/public/circuits/${CIRCUIT_NAME}_final.zkey
-cp ${FULL_PATH}_obj/verification_key.json ui/public/circuits/${CIRCUIT_NAME}_vkey.json
+cp ${FULL_PATH}_obj/${CIRCUIT_NAME}_js/${CIRCUIT_NAME}.wasm ../ui/public/circuits/${CIRCUIT_NAME}.wasm
+cp ${FULL_PATH}_obj/${CIRCUIT_NAME}_final.zkey ../ui/public/circuits/${CIRCUIT_NAME}_final.zkey
